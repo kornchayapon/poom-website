12c0c8ef3da66cc42e70e0e68227f2c779d9a25d
natgooddev <gooddev639@gmail.com>
2019-02-16 10:18:56 +0700

    basic restful api server

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,40 @@
-console.log('Hello nodejs server II')
+let express = require('express')
+
+const app = express()
+
+let port = 8080
+
+app.get('/status', function (req, res) {
+  res.send('Hello nodejs server')
+})
+
+// user section
+// create
+app.post('/user', function (req, res) {
+  res.send('OK I created user')
+})
+
+// update user
+app.put('/user/:userId', function (req, res) {
+  res.send('OK I updated user')
+})
+
+// delete user
+app.delete('/user/:userId', function (req, res) {
+  res.send('OK I deleted user')
+})
+
+// view all user
+app.get('/users', function (req, res) {
+  res.send('get all user')
+})
+
+// view user
+app.get('/user/:userId', function (req, res) {
+  res.send('get user by id')
+})
+
+
+app.listen(port, function () {
+  console.log('server running on ' + port)
+})
